fb3e1ef9cf16c4158494f6eedf61d0afa773e241
Marc Anton Dahmen <mail@marcdahmen.de>
2019-01-06 19:10:49 +0100

    Added page_not_found template

---
+++ page_not_found.php
@@ -0,0 +1,13 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+<@ snippets/header.php @>
+
+	<@ snippets/carousel_full_width.php @>
+	
+	<div class="overflow-hidden">
+		<div class="container py-5">
+			<h1>404</h1>
+			<h2>Page Not Found</h2>
+		</div>
+	</div>
+	
+<@ snippets/footer.php @> 
