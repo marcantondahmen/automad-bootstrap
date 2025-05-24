cac31a4c3c5e7b5a9877c32c50e49431aec5422d
Marc Anton Dahmen <mail@marcdahmen.de>
2025-05-24 11:38:04 +0200

    feat: use new meta tags engine in Automad 2

--- snippets/header.php
+++ snippets/header.php
@@ -2,17 +2,19 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<@ set {
-		metaTitle: "@{ sitename } / @{ title | def ('404') }"
-	} @>
-    <title>@{ metaTitle | stripTags }</title>
-	<@ Automad/MetaTags {
-		description: @{ textTeaser | stripTags },
-		ogTitle: @{ metaTitle | stripTags },
-		ogDescription: @{ textTeaser | stripTags },
-		ogImage: @{ ogImage },
-		twitterCard: 'summary_large_image'
-	} @>
+	<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
+		<@ set {
+			metaTitle: "@{ sitename } / @{ title | def ('404') }"
+		} @>
+		<title>@{ metaTitle | stripTags }</title>
+		<@ Automad/MetaTags {
+			description: @{ textTeaser | stripTags },
+			ogTitle: @{ metaTitle | stripTags },
+			ogDescription: @{ textTeaser | stripTags },
+			ogImage: @{ ogImage },
+			twitterCard: 'summary_large_image'
+		} @>
+	<?php } ?>
 	<@ with @{ favicon } @><link href="@{ :file }" rel="shortcut icon" type="image/x-icon" /><@ end @>
 	<@ with @{ appleTouchIcon } @><link href="@{ :file }" rel="apple-touch-icon" /><@ end @>
 	<link rel="stylesheet" href="@{ bootstrapCss | def ('/vendor/twbs/bootstrap/dist/css/bootstrap.min.css') }" />
